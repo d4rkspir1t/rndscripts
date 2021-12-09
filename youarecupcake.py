67096c4e163086f7f165e312d0589f1d36553fc3
d4rkspir1t <svmotric@gmail.com>
2021-12-09 18:21:56 +0000

    Create youarecupcake.py
    rnd script

---
+++ youarecupcake.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+
+def fetch(arguments,index):
+	try:
+		res = arguments[index]
+	except IndexError:
+		res = 0
+	return res
+
+lv = 'Cupcake'
+
+arguments =[item.lower() for item in sys.argv[1:]]
+if fetch(arguments, 0) == "love":
+    if fetch(arguments, 1) == lv:
+        print('Of course, she\'s so cool even The Economist wants her')
+    else:
+        print('No! You love %s. SAYIT!' % (lv.capitalize()))
+else:
+    print('C\'mon, talk about love, brain. I know it\'s hard to get out.')
